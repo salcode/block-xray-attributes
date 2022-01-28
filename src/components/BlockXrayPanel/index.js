59484ad10959a2b53e09585db503cc5372186220
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-01-28 14:00:38 -0500

    Use SelectedBlockAttributes in BlockXrayPanel
    Use SelectedBlockAttributes component in BlockXrayPanel

--- src/components/BlockXrayPanel/index.js
+++ src/components/BlockXrayPanel/index.js
@@ -1,12 +1,14 @@
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 
+import SelectedBlockAttributes from '../SelectedBlockAttributes';
+
 export default function BlockXrayPanel() {
   return (
     <PluginDocumentSettingPanel
       icon="code-standards"
       title="Block X-ray"
     >
-      <p>Sidebar content for Block X-ray Attributes plugin</p>
+      <SelectedBlockAttributes />
     </PluginDocumentSettingPanel>
   );
 }
