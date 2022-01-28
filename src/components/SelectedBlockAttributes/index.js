430e351453c0aa2fe78a406d092a79ec1aa88d4e
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-01-28 14:18:23 -0500

    Add message when no block is selected

--- src/components/SelectedBlockAttributes/index.js
+++ src/components/SelectedBlockAttributes/index.js
@@ -3,6 +3,9 @@ import { select } from '@wordpress/data';
 export default function SelectedBlockAttributes() {
   const selectedBlock = select('core/block-editor').getSelectedBlock();
   console.log(selectedBlock);
+  if (! selectedBlock) {
+    return <p>No block is selected.</p>;
+  }
   return (
     <p>Selected block attributes {selectedBlock?.name}</p>
   );
