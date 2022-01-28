29a970ce0fa1c2a288e51bb1a66dc7791f4ac357
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-01-28 14:36:00 -0500

    Add required prop "object" to JavaScriptObject

--- src/components/JavaScriptObject/index.js
+++ src/components/JavaScriptObject/index.js
@@ -1,3 +1,9 @@
+import PropTypes from 'prop-types';
+
 export default function JavaScriptObject() {
   return <h5>JavaScript Object</h5>;
 }
+
+JavaScriptObject.propTypes = {
+  object: PropTypes.object.isRequired,
+};
