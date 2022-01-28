9b25ce4c380256e4a258c0a430cf32db479bff02
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-01-28 13:28:05 -0500

    Register plugin block-xray-attributes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 console.log('salcode Hello from src/index.js');
 import { registerPlugin } from '@wordpress/plugins';
+
+registerPlugin(
+  'block-xray-attributes',
+  {
+    render: () => {
+      console.log('plugin block-xray-attributes render()');
+    }
+  },
+);
