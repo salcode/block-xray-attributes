7e2f633417bd87e281f9accc664788314f494ccc
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-01-28 14:05:50 -0500

    Select currentBlock and display block name
    Note: This code does not update when switching from one block to
    another. A select will not cause our information to stay up to date.
    
    We will need to update this code to use useSelect

--- src/components/SelectedBlockAttributes/index.js
+++ src/components/SelectedBlockAttributes/index.js
@@ -1,5 +1,9 @@
+import { select } from '@wordpress/data';
+
 export default function SelectedBlockAttributes() {
+  const selectedBlock = select('core/block-editor').getSelectedBlock();
+  console.log(selectedBlock);
   return (
-    <p>Selected block attributes</p>
+    <p>Selected block attributes {selectedBlock?.name}</p>
   );
 }
