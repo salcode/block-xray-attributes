92039e8398be52be691ac9d7fd036a148003c011
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-01-28 10:02:53 -0500

    Initial commit

---
+++ block-xray-attributes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name:       Block X-ray Attributes
+ * Plugin URI:        https://github.com/salcode/block-xray-attributes
+ * Description:       Shows the attributes of the current Gutenberg block in the Document sidebar.
+ * Version:           0.1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Sal Ferrarello
+ * Author URI:        https://salferrarello.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/salcode/block-xray-attributes
+ * Text Domain:       block-xray-attributes
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
