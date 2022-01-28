64bf7425967af35ae08e4ceb1558207e6e5869c7
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-01-28 14:36:50 -0500

    Remove console.log() from SelectedBlockAttributes

--- src/components/SelectedBlockAttributes/index.js
+++ src/components/SelectedBlockAttributes/index.js
@@ -6,7 +6,6 @@ export default function SelectedBlockAttributes() {
   const selectedBlock = useSelect(
     (select) => select('core/block-editor').getSelectedBlock()
   );
-  console.log(selectedBlock);
   if (! selectedBlock) {
     return <p>No block is selected.</p>;
   }
