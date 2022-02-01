9e899b41dc05b2150ca78a028ac5d50349fd3c7f
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-01-31 19:31:20 -0500

    Utilize HOC in BlockXrayPanel
    See #10

--- src/components/BlockXrayPanel/index.js
+++ src/components/BlockXrayPanel/index.js
@@ -2,14 +2,18 @@ import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
 
 import SelectedBlockAttributes from '../SelectedBlockAttributes';
+import WithSelectedBlock from '../../hocs/WithSelectedBlock';
 
 export default function BlockXrayPanel() {
+  const SelectedBlockAttributesWithBlock = WithSelectedBlock(
+    SelectedBlockAttributes,
+  );
   return (
     <PluginDocumentSettingPanel
       icon="code-standards"
       title={__( 'Block X-ray', 'block-xray-attributes' )}
     >
-      <SelectedBlockAttributes />
+      <SelectedBlockAttributesWithBlock />
     </PluginDocumentSettingPanel>
   );
 }
