edec563b93688bfae54edb8ae0357a4eb818a9ce
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-01-31 19:28:03 -0500

    Modify SelectedBlockAttributes to accept prop
    Modify SelectedBlockAttributes to accept prop block
    
    See #10

--- src/components/SelectedBlockAttributes/index.js
+++ src/components/SelectedBlockAttributes/index.js
@@ -1,18 +1,22 @@
-import { useSelect } from '@wordpress/data';
+import PropTypes from 'prop-types';
+
 import { __ } from '@wordpress/i18n';
 
 import JavaScriptObject from '../JavaScriptObject';
 
-export default function SelectedBlockAttributes() {
-  const selectedBlock = useSelect(
-    (select) => select('core/block-editor').getSelectedBlock()
-  );
-  if (! selectedBlock) {
+export default function SelectedBlockAttributes({
+  block = {},
+}) {
+  if (! block) {
     return <p>{__( 'No block is selected.', 'block-xray-attributes' )}</p>;
   }
   return (
     <JavaScriptObject
-      object={selectedBlock}
+      object={block}
     />
   );
 }
+
+SelectedBlockAttributes.propTypes = {
+  object: PropTypes.object,
+};
