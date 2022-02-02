a9dcec6741ed0c19c9ac9466ffe103497198ee1b
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-02-01 21:47:15 -0500

    Modify BlockXray to take "block" as a prop
    Modify BlockXray to take "block" as a prop, instead of doing the lookup
    inside the BlockXray component.

--- src/components/BlockXray/index.js
+++ src/components/BlockXray/index.js
@@ -1,18 +1,23 @@
-import { useSelect } from '@wordpress/data';
+import PropTypes from 'prop-types';
+
 import { __ } from '@wordpress/i18n';
 
 import JavaScriptObject from '../JavaScriptObject';
 
-export default function SelectedBlockAttributes() {
-  const selectedBlock = useSelect(
-    (select) => select('core/block-editor').getSelectedBlock()
-  );
-  if (! selectedBlock) {
+export default function BlockXray({
+  block = {},
+}) {
+  if (! block) {
     return <p>{__( 'No block is selected.', 'block-xray-attributes' )}</p>;
   }
+
   return (
     <JavaScriptObject
-      object={selectedBlock}
+      object={block}
     />
   );
+}
+
+BlockXray.propTypes = {
+  block: PropTypes.object,
 }
