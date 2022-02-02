93b20f6fd7ac726908e61a633d5c04987791c755
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-02-01 21:49:00 -0500

    Wrap BlockXray with HOC WithSelectedBlock

--- src/components/BlockXrayPanel/index.js
+++ src/components/BlockXrayPanel/index.js
@@ -2,6 +2,11 @@ import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
 
 import BlockXray from '../BlockXray';
+import WithSelectedBlock from '../../hocs/WithSelectedBlock';
+
+const BlockXrayWithSelectedBlock = WithSelectedBlock(
+  BlockXray,
+);
 
 export default function BlockXrayPanel() {
   return (
@@ -9,7 +14,7 @@ export default function BlockXrayPanel() {
       icon="code-standards"
       title={__( 'Block X-ray', 'block-xray-attributes' )}
     >
-      <BlockXray />
+      <BlockXrayWithSelectedBlock />
     </PluginDocumentSettingPanel>
   );
 }
