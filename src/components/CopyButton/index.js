ec128d7c8cf7812a7a34a358eaacbe6f665b6847
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-02-25 14:48:05 -0500

    Add copy to clipboard to CopyButton
    As outlined in the GitHub issue for this feature, there are three
    different options available for adding this functionality in Gutenberg.
    
    I have opted for the oldest (and now deprecated ClipboardButton
    component).
    
    This does come with it the disadvantage of a depreaction notice but that
    seems a small price to pay for backwards compatibility.
    
    Initially, I inteneded to use the most modern solution
    useCopyToClipboard (adding in Gutenberg 10.3.0, WordPress core 5.8.x),
    my thinking being that since we were importing this hook from the latest
    version of the package, it would work despite running on an older
    version of Gutenberg.
    
    import { useCopyToClipboard } from '@wordpress/compose';
    
    Nope - this fails with with the following error
    
    TypeError
    _wordpress_composer__WEBPACK_IMPORTED_MODULE_1__.useCopyToClipboard is
    not a function
    
    This only deepens my confusion around importing packages vs
    destructuring them from the "wp" global as discussed in
    https://salferrarello.com/wordpress-gutenberg-import-versus-destructuring-global-wp/
    
    At this point I could add a conditional and provide two (or more) sets
    of code to create this copy functionality to ensure I don't throw any
    deprecation notices or I can use this older (but fully functional
    technique) and be done with it.
    
    Resolves #17

--- src/components/CopyButton/index.js
+++ src/components/CopyButton/index.js
@@ -1,16 +1,28 @@
 import PropTypes from 'prop-types';
 
+import { ClipboardButton } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 export default function CopyButton({
   content,
 }) {
+  const [ hasCopied, setHasCopied ] = useState( false );
   return (
-    <button>
-      {__( 'Copy Block Data', 'block-xray-attributes' )}
-    </button>
+    <ClipboardButton
+    className="is-primary"
+    text={content}
+    onCopy={ () => setHasCopied( true ) }
+    onFinishCopy={ () => setHasCopied( false ) }
+    >
+      {
+        hasCopied ?
+        __( 'Copied!', 'block-xray-attributes' ) :
+        __( 'Copy Block Data', 'block-xray-attributes' )
+      }
+    </ClipboardButton>
   );
-}
+};
 
 CopyButton.propTypes = {
   content: PropTypes.string.isRequired,
