871444b582a912239d2506c6cb69e8b4f536e2b4
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-02-25 14:47:45 -0500

    Add content prop to CopyButton

--- src/components/CopyButton/index.js
+++ src/components/CopyButton/index.js
@@ -1,9 +1,17 @@
+import PropTypes from 'prop-types';
+
 import { __ } from '@wordpress/i18n';
 
-export default function CopyButton() {
+export default function CopyButton({
+  content,
+}) {
   return (
     <button>
       {__( 'Copy Block Data', 'block-xray-attributes' )}
     </button>
   );
 }
+
+CopyButton.propTypes = {
+  content: PropTypes.string.isRequired,
+};
