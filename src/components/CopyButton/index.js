47726d3dd763c141fcde3c0e4c8d37384c195628
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-02-25 14:48:05 -0500

    Add copy to clipboard to CopyButton
    Resolves #17

--- src/components/CopyButton/index.js
+++ src/components/CopyButton/index.js
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types';
 
+import { useCopyToClipboard } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
 
 export default function CopyButton({
   content,
 }) {
+  const ref = useCopyToClipboard(content);
   return (
-    <button>
+    <button
+      ref={ref}
+    >
       {__( 'Copy Block Data', 'block-xray-attributes' )}
     </button>
   );
