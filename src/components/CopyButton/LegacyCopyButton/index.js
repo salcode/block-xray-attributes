7c920c4e1083aa32c5392678da040663f47c9c06
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-03-28 16:54:52 -0400

    Move logic to LegacyCopyButton component

---
+++ src/components/CopyButton/LegacyCopyButton/index.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+import { ClipboardButton } from '@wordpress/components';
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * This component uses the now deprecated ClipboardButton component
+ * but it provides support back to WordPress 5.4.x (Gutenberg 6.7.0).
+ */
+export default function LegacyCopyButton({
+  content,
+}) {
+  const [ hasCopied, setHasCopied ] = useState( false );
+  return (
+    <ClipboardButton
+      className="is-primary"
+      text={content}
+      onCopy={ () => setHasCopied( true ) }
+      onFinishCopy={ () => setHasCopied( false ) }
+    >
+      {
+        hasCopied ?
+        __( 'Copied!', 'block-xray-attributes' ) :
+        __( 'Copy Block Data', 'block-xray-attributes' )
+      }
+    </ClipboardButton>
+  );
+}
+
+LegacyCopyButton.propTypes = {
+  content: PropTypes.string.isRequired,
+};
--- src/components/CopyButton/index.js
+++ src/components/CopyButton/index.js
@@ -1,26 +1,14 @@
 import PropTypes from 'prop-types';
 
-import { ClipboardButton } from '@wordpress/components';
-import { useState } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
+import LegacyCopyButton from './LegacyCopyButton';
 
 export default function CopyButton({
   content,
 }) {
-  const [ hasCopied, setHasCopied ] = useState( false );
   return (
-    <ClipboardButton
-    className="is-primary"
-    text={content}
-    onCopy={ () => setHasCopied( true ) }
-    onFinishCopy={ () => setHasCopied( false ) }
-    >
-      {
-        hasCopied ?
-        __( 'Copied!', 'block-xray-attributes' ) :
-        __( 'Copy Block Data', 'block-xray-attributes' )
-      }
-    </ClipboardButton>
+    <LegacyCopyButton
+      content={content}
+    />
   );
 };
 
