614764fadb2248aa76da5884e8a204e89b34e5c3
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-04-29 12:00:14 -0400

    Add built files

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'eee62009ed536b4910448da5e3781945');
