8058bee0b991f8a69b3449ea0ff439ca353be74f
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2025-02-16 09:01:36 -0500

    Add sidebar panel to WithBlockXrayPanel HOC

--- src/hocs/WithBlockXrayPanel/index.js
+++ src/hocs/WithBlockXrayPanel/index.js
@@ -1,9 +1,30 @@
+/* global wp */
+
+const {
+  components: {
+    PanelBody,
+  },
+  editor: {
+    InspectorControls,
+  },
+  i18n: {
+    __,
+  },
+} = wp;
+
 export default function WithBlockXrayPanel(WrappedComponent) {
   return props => {
     return (
-      <WrappedComponent
-        {...props}
-      />
+      <>
+        <WrappedComponent
+          {...props}
+        />
+        <InspectorControls>
+          <PanelBody title={__('Xray', 'example')}>
+            <h2>salcode</h2>
+          </PanelBody>
+        </InspectorControls>
+      </>
     );
   };
 }
